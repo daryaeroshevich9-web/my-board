76b8235832aaa1f9c7210ba272648fed34069d13
daryaeroshevich9-web <daryaeroshevich9@gmail.com>
2026-09-20 14:20:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE = 'darya-board-v4';
+const CACHE = 'darya-board-v5';
 const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
