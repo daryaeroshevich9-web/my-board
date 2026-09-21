4c275c64820b082f06a7378f8a16be3d76e8c64c
daryaeroshevich9-web <daryaeroshevich9@gmail.com>
2026-09-21 11:10:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'darya-board-v5';
-const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
+const CACHE = 'darya-board-v6';
+const SHELL = ['./', './index.html', './styles.css', './app.js', './flight.js', './manifest.json', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
 });
