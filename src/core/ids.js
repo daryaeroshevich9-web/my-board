efc91f3bcf845939bdbc038866b7f9106a51a693
daryaeroshevich9-web <daryaeroshevich9@gmail.com>
2026-09-22 14:34:43 +0300

    Update ids.js

--- src/core/ids.js
+++ src/core/ids.js
@@ -1,4 +1,4 @@
-/* board v3.0 stage-0 */
+/* board v3.0 stage-1 */
 
 export const IDS = {
   app: 'app',
@@ -9,10 +9,43 @@ export const IDS = {
   settingsOpen: 'settingsOpen',
   backupDownload: 'backupDownload',
 
+  quickAddInput: 'quickAddInput',
+  quickAddSubmit: 'quickAddSubmit',
+  boardSummary: 'boardSummary',
+
   settingsModal: 'settingsModal',
   settingsTitle: 'settingsTitle',
   settingsClose: 'settingsClose',
   settingsToken: 'settingsToken',
   settingsSave: 'settingsSave',
   onboardingNote: 'onboardingNote',
+
+  confirmModal: 'confirmModal',
+  confirmTitle: 'confirmTitle',
+  confirmMessage: 'confirmMessage',
+  confirmCancel: 'confirmCancel',
+  confirmOk: 'confirmOk',
+
+  zonesModal: 'zonesModal',
+  zonesTitle: 'zonesTitle',
+  zonesClose: 'zonesClose',
+  zonesList: 'zonesList',
+  zoneNewLabel: 'zoneNewLabel',
+  zoneCreateBtn: 'zoneCreateBtn',
+
+  archiveModal: 'archiveModal',
+  archiveTitle: 'archiveTitle',
+  archiveClose: 'archiveClose',
+  archiveSearch: 'archiveSearch',
+  archiveList: 'archiveList',
+
+  quickZoneModal: 'quickZoneModal',
+  quickZoneTitle: 'quickZoneTitle',
+  quickZoneClose: 'quickZoneClose',
+  quickZoneList: 'quickZoneList',
+
+  dueModal: 'dueModal',
+  dueTitle: 'dueTitle',
+  dueClose: 'dueClose',
+  dueDateInput: 'dueDateInput',
 };
