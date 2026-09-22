c2bc1638743d82cd8a9370efa74a45ee2b135576
daryaeroshevich9-web <daryaeroshevich9@gmail.com>
2026-09-22 14:09:46 +0300

    Create ids.js

---
+++ src/core/ids.js
@@ -0,0 +1,18 @@
+/* board v3.0 stage-0 */
+
+export const IDS = {
+  app: 'app',
+  sidebar: 'sidebar',
+  board: 'board',
+  toast: 'toast',
+
+  settingsOpen: 'settingsOpen',
+  backupDownload: 'backupDownload',
+
+  settingsModal: 'settingsModal',
+  settingsTitle: 'settingsTitle',
+  settingsClose: 'settingsClose',
+  settingsToken: 'settingsToken',
+  settingsSave: 'settingsSave',
+  onboardingNote: 'onboardingNote',
+};
